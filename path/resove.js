bec509663da08942df54540ee0fc704c19f250d5
Zhang <A2024452655@outlook.com>
2023-03-17 20:23:13 +0800

    resolve

---
+++ path/resove.js
@@ -0,0 +1,8 @@
+const path = require('path')
+console.log(path.resolve(__dirname, "index.html"));
+console.log(path.resolve(__dirname, './index.html'));
+//上面打印都是C:\Users\Zha\Desktop\vue ajax\node1\path\index.html
+
+//不要直接/index.html
+console.log(path.resolve(__dirname, '/index.html'));
+//上面打印的是C:\index.html
